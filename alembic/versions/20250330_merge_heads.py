539d4e062d719e01b3f42fa44b2f6c2ecd5ab801
Dave Stanton <davestanton.us@gmail.com>
2025-03-30 15:40:33 -0400

    Linting.

--- alembic/versions/20250330_merge_heads.py
+++ alembic/versions/20250330_merge_heads.py
@@ -8,8 +8,6 @@ Create Date: 2025-03-30 10:30:00.000000
 
 from typing import Sequence, Union
 
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision: str = "20250330_merge"
 down_revision: Union[str, Sequence[str], None] = ("8ccc8e52ee58", "20250330")
@@ -19,9 +17,7 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Merge the two heads."""
-    pass
 
 
 def downgrade() -> None:
     """No downgrade needed for merge."""
-    pass
